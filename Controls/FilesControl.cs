8f1873d3881043bb5be349a85e8d59ee52f2fd0c
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-06-05 17:14:34 -0300

    Add: Summary

--- Controls/FilesControl.cs
+++ Controls/FilesControl.cs
@@ -12,7 +12,13 @@ namespace PdfGenerationHelper.Controls
 {
     class FilesControl
     {
-
+        /// <summary>
+        /// Add a text to the document
+        /// </summary>
+        /// <param name="document">Document that text will be add.</param>
+        /// <param name="text">String with the text.</param>
+        /// <param name="fontSize">Font size.</param>
+        /// <param name="alignment">Text alignment.</param>
         public static void AddText(ref Document document, string text, int fontSize, TextAlignment alignment)
         {
             document.Add(new Paragraph(text)
