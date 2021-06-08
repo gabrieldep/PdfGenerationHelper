fab9d1bf29f8a96915b17a6b5c106692cc7288e7
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-06-07 21:21:18 -0300

    Add: BackgroundEventHandler

---
+++ IEventHandlers/BackgroundEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using iText.Kernel.Colors;
+using iText.Kernel.Events;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfGenerationHelper.IEventHandlers
+{
+    public class BackgroundEventHandler : IEventHandler
+    {
+        private readonly Color _backgroundColor;
+        public BackgroundEventHandler(Color backgroundColor)
+        {
+            _backgroundColor = backgroundColor;
+        }
+
+        public void HandleEvent(Event @event)
+        {
+            PdfDocumentEvent docEvent = (PdfDocumentEvent)@event;
+            PdfDocument pdfDoc = docEvent.GetDocument();
+            PdfPage page = docEvent.GetPage();
+            Rectangle pageSize = page.GetPageSize();
+            PdfCanvas pdfCanvas = new(page.NewContentStreamBefore(), page.GetResources(), pdfDoc);
+            pdfCanvas.SaveState()
+                        .SetFillColor(_backgroundColor)
+                        .Rectangle(pageSize.GetLeft(), pageSize.GetBottom(), pageSize.GetWidth(), pageSize.GetHeight())
+                        .Fill()
+                        .RestoreState();
+        }
+    }
+}
