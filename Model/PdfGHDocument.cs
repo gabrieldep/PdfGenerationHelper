3b4562cf8901e7516a08889651bda2d35a57c537
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-06-17 16:09:03 -0300

    Add: Image can be added

--- Model/PdfGHDocument.cs
+++ Model/PdfGHDocument.cs
@@ -1,4 +1,5 @@
-﻿using iText.Kernel.Colors;
+﻿using iText.IO.Image;
+using iText.Kernel.Colors;
 using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
@@ -55,6 +56,11 @@ namespace PdfGenerationHelper.Model
             Document.Close();
         }
 
+        /// <summary>
+        /// Fill a table with the data received and add to a document
+        /// </summary>
+        /// <param name="objects">IEnumerable with the objects to complete the table.</param>
+        /// <returns>Return a filled table.</returns>
         public void AddTable<T>(IEnumerable<T> objects)
         {
             Type type = objects.First().GetType();
@@ -64,7 +70,8 @@ namespace PdfGenerationHelper.Model
                 .Where(p => p.PropertyType.Namespace == "System");
 
             Table table = new(properties.Count());
-
+            table.SetHorizontalAlignment(HorizontalAlignment.CENTER);
+            table.SetWidth(PdfDocument.GetPage(1).GetPageSize().GetWidth() * 9/10);
             IEnumerable<PdfObject<T>> ListPdfObjects = objects
                 .Select(o => new PdfObject<T>
                 {
@@ -91,6 +98,17 @@ namespace PdfGenerationHelper.Model
                 }
             }
             Document.Add(table);
+        }
+
+        /// <summary>
+        /// Add a image to the document
+        /// </summary>
+        /// <param name="path">String with image name.</param>
+        public void AddImage(string path)
+        {
+            ImageData imageData = ImageDataFactory.Create(path);
+            Image image = new(imageData);
+            Document.Add(image);
         }
 
         public Document Document { get; set; }
